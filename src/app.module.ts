6167918d50bdb63ff9bcf683d2705de346e261e8
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2022-08-21 15:48:57 -0500

    Added web socket

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { AuthModule } from './auth/auth.module';
     SeedModule,
     FilesModule,
     AuthModule,
+    MessagesWsModule,
   ],
   controllers: [],
   providers: [],
